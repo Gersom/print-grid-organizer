09364dc797b1902656156b350343275cd2748ec4
Gersom <gersomalaja@gmail.com>
2025-12-03 15:01:54 -0500

    refactor: rename PIXELS_PER_MM_AT_300DPI constant to MM_TO_PX
    The new name is more concise while still being descriptive of the constant's purpose

--- src/main.js
+++ src/main.js
@@ -43,7 +43,7 @@ const PAPER_SIZES = {
     custom: { width: 210, height: 297 }
 };
 
-const PIXELS_PER_MM_AT_300DPI = 11.811023622; // Conversion factor from millimeters to pixels at 300 DPI
+const MM_TO_PX = 11.811023622; // Conversion factor from millimeters to pixels at 300 DPI
 
 /**
  * Initialize event listeners
